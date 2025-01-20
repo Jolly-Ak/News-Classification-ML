d069ac58f464ca331db25b1cfb8f0890d865f81b
azimusk40 <a.platon313@gmail.com>
2025-01-20 11:39:54 +0100

    anas

--- ClassificationAutomatique/src/Categorie.java
+++ ClassificationAutomatique/src/Categorie.java
@@ -1,4 +1,5 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -30,7 +31,59 @@ public class Categorie {
     }
 
 
-    //calcul du score d'une dépêche pour la catégorie
+    import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
+    public class PaireChaineEntier {
+        private String chaine;
+        private int entier;
+
+        public PaireChaineEntier(String chaine, int entier) {
+            this.chaine = chaine;
+            this.entier = entier;
+        }
+
+
+
+        public void initLexique (String nomFichier) throws FileNotFoundException {
+            ArrayList<PaireChaineEntier> lexique = new ArrayList<>();
+
+
+            try {
+                // lecture du fichier d'entrée
+                FileInputStream file = new FileInputStream(nomFichier);
+                Scanner scanner = new Scanner(file);
+
+                while (scanner.hasNextLine()) {
+                    String ligne = scanner.nextLine();
+                    int index = ligne.indexOf(":");
+                    String chaine = ligne.substring(0, index);
+                    int entier = Integer.parseInt(ligne.substring(index + 1));
+                    ligne = scanner.nextLine();
+
+
+                    lexique.add(new PaireChaineEntier(chaine, entier));
+
+                }
+                scanner.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+    }
+
+}
+
+
+
+
+
+//calcul du score d'une dépêche pour la catégorie
     public int score(Depeche d) {
         return 0;
     }
--- ClassificationAutomatique/src/PaireChaineEntier.java
+++ ClassificationAutomatique/src/PaireChaineEntier.java
@@ -1,13 +1,17 @@
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
 public class PaireChaineEntier {
     private String chaine;
     private int entier;
 
-
-
-    public void initLexique (String nomFichier){
-        
+    public PaireChaineEntier(String chaine, int entier) {
+        this.chaine = chaine;
+        this.entier = entier;
     }
-
 }
+
 
 
