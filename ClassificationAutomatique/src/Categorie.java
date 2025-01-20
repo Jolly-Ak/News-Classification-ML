b5927072c6da108c7299ac2f973cc18cb083f4fc
azimusk40 <a.platon313@gmail.com>
2025-01-20 11:55:03 +0100

    anas make

--- ClassificationAutomatique/src/Categorie.java
+++ ClassificationAutomatique/src/Categorie.java
@@ -12,6 +12,7 @@ public class Categorie {
     // constructeur
     public Categorie(String nom) {
         this.nom = nom;
+        this.lexique = new ArrayList<PaireChaineEntier>();
     }
 
 
@@ -20,73 +21,45 @@ public class Categorie {
     }
 
 
-    public  ArrayList<PaireChaineEntier> getLexique() {
+    public ArrayList<PaireChaineEntier> getLexique() {
         return lexique;
     }
 
 
     // initialisation du lexique de la catégorie à partir du contenu d'un fichier texte
+
     public void initLexique(String nomFichier) {
 
-    }
+
+        try {
+            // lecture du fichier d'entrée
+            FileInputStream file = new FileInputStream(nomFichier);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String ligne = scanner.nextLine();
+                int index = ligne.indexOf(":");
+                String chaine = ligne.substring(0, index);
+                int entier = Integer.parseInt(ligne.substring(index + 1));
+                ligne = scanner.nextLine();
 
 
-    import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.io.*;
+                lexique.add(new PaireChaineEntier(chaine, entier));
 
-    public class PaireChaineEntier {
-        private String chaine;
-        private int entier;
-
-        public PaireChaineEntier(String chaine, int entier) {
-            this.chaine = chaine;
-            this.entier = entier;
-        }
-
-
-
-        public void initLexique (String nomFichier) throws FileNotFoundException {
-            ArrayList<PaireChaineEntier> lexique = new ArrayList<>();
-
-
-            try {
-                // lecture du fichier d'entrée
-                FileInputStream file = new FileInputStream(nomFichier);
-                Scanner scanner = new Scanner(file);
-
-                while (scanner.hasNextLine()) {
-                    String ligne = scanner.nextLine();
-                    int index = ligne.indexOf(":");
-                    String chaine = ligne.substring(0, index);
-                    int entier = Integer.parseInt(ligne.substring(index + 1));
-                    ligne = scanner.nextLine();
-
-
-                    lexique.add(new PaireChaineEntier(chaine, entier));
-
-                }
-                scanner.close();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-
-
     }
 
-}
-
-
-
-
-
-//calcul du score d'une dépêche pour la catégorie
+    //calcul du score d'une dépêche pour la catégorie
     public int score(Depeche d) {
         return 0;
     }
 
 
 }
+
+
+
